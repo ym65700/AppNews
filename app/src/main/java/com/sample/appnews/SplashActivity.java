20671880048d0c9411f5346766500adb639ad11d
xiaoming <657006533@qq.com>
2017-05-24 13:33:11 +0800

    my first commit

---
+++ app/src/main/java/com/sample/appnews/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.sample.appnews;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+    p
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+    }
+}
